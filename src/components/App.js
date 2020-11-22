a3bee7cfbbcc0c520d3fda2ffeb70fe9634ebedb
Damian <themrdamus@gmail.com>
2020-11-22 17:18:34 +0000

    fetching data

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,75 @@ import Form from './Form';
 import Result from './Result';
 import './App.css';
 
+const APIKey = 'f27143c9ba3302311a2c4596d74c5dc0'
+
 class App extends Component {
 
   state = {
-    value: "sting"
+    value: '',
+    date: '',
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    pressure: '',
+    wind: '',
+    err: false
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleCitySubmit = e => {
+    e.preventDefault()
+    const API = 
+    `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}&units=metric`;
+
+    fetch(API)
+    .then(res => {
+      if(res.ok){
+        return res
+      }
+      throw Error("failed to get data")
+    })
+    .then(res => res.json())
+    .then(data => {
+      const time = new Date().toLocaleDateString()
+      this.setState(prevState => ({
+        err: false,
+        date: time,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        temp: data.main.temp,
+        pressure: data.main.pressure,
+        wind: data.wind.speed,
+        city: prevState.value,
+      }))
+    })
+    .catch(err => {
+      console.log(err)
+      this.setState(prevState => ({
+        err: true,
+        city: prevState.value
+      }))
+    })
+
+
   }
 
   render() {
     return (
       <div className="App">
         Weather App
-        <Form text={this.state.value}/>
-        <Result />
+        <Form 
+        value={this.state.value} 
+        change={this.handleInputChange}
+        submit={this.handleCitySubmit}
+        />
+        <Result weather={this.state}/>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Form = props => {
   return (
-    <form>
+    <form onSubmit={props.submit}>
       <input
         type="text" 
-        value={props.value}/>
+        value={props.value}
+        onChange={props.change}
+        placeholder="enter city name"
+        />
       <button>search city</button>
     </form>
   )
