c9e21f01dbedc1f4c16a5abf01da74b587b77fdb
Damian <themrdamus@gmail.com>
2020-11-28 20:14:24 +0000

    layout, styling and rwd

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './form.css';
 
 const Form = props => {
   return (
     <form>
       <input
+        className="search-box"
         type="text" 
         value={props.value}
         onChange={props.change}
